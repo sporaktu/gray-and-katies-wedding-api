6d817ac0dd6bd4681a86d5de5112c595a0bbac63
Gray Karegeannes <11544089+sporaktu@users.noreply.github.com>
2020-09-02 00:53:23 -0400

    removing ssl

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const pool = new Pool({
         isProduction ?
             process.env.DATABASE_URL :
             connectionString,
-    ssl: isProduction
+    ssl: false
 });
 
 module.exports = {pool};
