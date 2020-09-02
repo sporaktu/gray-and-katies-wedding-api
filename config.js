44852a7dd0f673bc7dceb4005e6e70f68ecdb89e
Gray Karegeannes <grayson.karegeannes@nextraq.com>
2020-09-02 15:04:12 -0400

    more logs

--- config.js
+++ config.js
@@ -6,12 +6,15 @@ console.log(isProduction, process.env.DATABASE_URL);
 const connectionString =
     `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const pool = new Pool({
+const poolConfig = {
     connectionString:
-        isProduction ?
-            process.env.DATABASE_URL :
-            connectionString,
+      isProduction ?
+        process.env.DATABASE_URL :
+        connectionString,
     ssl: isProduction
-});
+}
+
+console.log(poolConfig);
+const pool = new Pool(poolConfig);
 
 module.exports = {pool};
