259689c6d4a82a7acbd40822cd530f7685139f06
Gray Karegeannes <11544089+sporaktu@users.noreply.github.com>
2020-09-02 00:57:50 -0400

    adding db url log

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const {Pool} = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
-
+console.log(process.env.DATABASE_URL);
 const connectionString =
     `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
