febe1da58051b607ca6690909f9e281a4f51ddf9
Gray Karegeannes <grayson.karegeannes@nextraq.com>
2020-09-02 01:58:03 -0400

    adding cert route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/.well-known/acme-challenge/lcKloZ061Zpg9vLA0BuK6ERQvCyZHXJawlz6JYCQNtY', (req, res) => {
+  res.send('lcKloZ061Zpg9vLA0BuK6ERQvCyZHXJawlz6JYCQNtY.sqRiGlYzKX-lkad7BbUvSBIj-39VGNACkgBFUekxzkU');
+})
+
 module.exports = router;
